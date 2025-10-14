b7276a18fbb1f3ed33a15e57c7b6d7714a55db70
muhamad satria <114433514+mhmdsatria@users.noreply.github.com>
2025-10-14 23:50:19 +0700

    Delete core directory

--- core/Hook.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if ($this->request->action != 'index') {
-		$this->rendered = true;
-		$this->jsonRender = true;
-		header('Content-Type: application/json');
-	}
-
-?>
--- core/Includer.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	require REQU.DS.'sql'.DS.'Includer.php';
-	require 'Session.php';
-	require 'Dispatcher.php';
-	require 'Controller.php';
